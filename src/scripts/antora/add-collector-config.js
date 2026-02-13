f4f0c2d6f85b676dbe6f5305ee1b99a04da1de36
Bart Kleijngeld <bartkl@gmail.com>
2026-02-14 00:28:24 +0100

    Werkt met enkel justfile lijkt het

--- src/scripts/antora/add-collector-config.js
+++ src/scripts/antora/add-collector-config.js
@@ -1,22 +1,16 @@
 const fs = require('fs')
-const path = require('path')
-
 
 module.exports.register = function () {
   this.once('contentAggregated', ({ contentAggregate, playbook }) => {
     for (const { origins } of contentAggregate) {
       for (const origin of origins) {
-        const outDir = path.resolve("./output")
-
         let collector = {
           run: {
-            command: `/opt/dataproducten/generate-documentation.sh`,
-            env: [{ name: "OUTDIR",
-                    value: outDir}]
+              command: `just generate-and-assemble`,
           },
           scan: {
             clean: true,
-            dir: path.join(outDir, "docs/adoc")
+            dir: "output/docs/adoc"
           }
 
         }
